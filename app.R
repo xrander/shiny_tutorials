f7dffb9a3c931e7f49643f71643b62de574e2fc2
xrander <olamideadu@gmail.com>
2023-12-14 15:48:38 +0100

    exercise 5 solution

--- app.R
+++ app.R
@@ -1,28 +1,31 @@
 library("shiny")
 library("tidyverse")
 
-## User Interface
+datasets <- c("economics", "faithfuld", "seals")
 
+
+## User Interface
 ui <- fluidPage(
-  sliderInput("x", label = "if x is", min = 1, max = 50, value = 30),
-  "then x times 5 is",
-  sliderInput("y", label ="and y is", min = 1, max = 50, value = 5),
-  "then (x * y) is", textOutput("product"),
-  "and (x * y) + 5 is", textOutput("product_plus5"),
-  "and (x * y) + 10 is", textOutput("product_plus10")
+  selectInput("dataset", "Dataset", choices = datasets),
+  verbatimTextOutput("summary"),
+  plotOutput("plot")
 )
 
 
 ## Server
 server <- function(input, output, session){
-  product <- reactive(input$x * input$y)
+  dataset <- reactive({
+    get(input$dataset, "package:ggplot2")
+  })
   
-  output$product<- renderText( product() )
+  output$summary<- renderPrint( 
+    summary(dataset())
+    )
 
-  output$product_plus5 <- renderText( product() + 5 )
-  
-  output$product_plus10 <- renderText( product() + 10 )
-}
+  output$plot <- renderPlot(
+    plot(dataset())
+  ,res = 96)
+  }
 
 
 ## Shiny app construction
